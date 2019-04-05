9be9df3ddcc3753afa3623e496c6b45d358bc18e
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2019-04-04 22:41:32 -0700

    First project prototype

--- script.js
+++ script.js
@@ -2,9 +2,25 @@
 // numerical.addEventListener('click', toggle);
 
 // const buttons = document.getElementsByTagName("button");
-const wrapper = document.querySelector('.chart');
+// const wrapper = document.querySelector('.chart');
 
-animateCSSGrid.wrapGrid(wrapper, { duration: 400 });
+// animateCSSGrid.wrapGrid(wrapper, { duration: 400 });
+
+let names = '';
+const elements = document.querySelectorAll('[class*="capacity"]');
+
+Array.from(elements).forEach(element => {
+  element.addEventListener('mouseover', showNames, false);
+  element.addEventListener('mouseout', removeNames, false);
+})
+
+function showNames (event) {
+  console.log(event.target.dataset.name);
+}
+
+function removeNames (event) {
+
+}
 
 function toggle(event) {
   const className = event.target.id;
